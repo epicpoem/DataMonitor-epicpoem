cf597085f578b1c885485302c4891664d98a29ef
Hongwon Kim <epicpoem@gmail.com>
2026-06-12 12:55:41 +0900

    [AI-Fix] 콘솔 한글 출력 인코딩 깨짐 수정
    - main.cpp: SetConsoleOutputCP/SetConsoleCP(CP_UTF8) 추가
    - DataMonitor.vcxproj: /utf-8 컴파일러 플래그 추가 (전체 구성)
    - DataMonitorTest.vcxproj: /utf-8 컴파일러 플래그 추가

--- DataMonitor/DataMonitor/main.cpp
+++ DataMonitor/DataMonitor/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <memory>
+#include <windows.h>
 #include "controller/MonitorController.h"
 #include "repository/JsonSampleRepository.h"
 
 int main(int argc, char* argv[]) {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
     const std::string dataPath = (argc > 1) ? argv[1] : "data/samples.json";
 
     try {
